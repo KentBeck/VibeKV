4959ae87a23c6abc7fb315b4bc0aa2560479ef8b
Kent Beck <kentlbeck@gmail.com>
2025-04-15 17:08:46 -0700

    Refactor tests to pass Store directly instead of using a factory

--- store/memory_test.go
+++ store/memory_test.go
@@ -7,27 +7,23 @@ import (
 // TestMemoryStorePut tests the Put operation for MemoryStore
 func TestMemoryStorePut(t *testing.T) {
 	store := NewMemoryStore()
-	defer store.Close()
 	TestPut(t, store)
 }
 
 // TestMemoryStoreGet tests the Get operation for MemoryStore
 func TestMemoryStoreGet(t *testing.T) {
 	store := NewMemoryStore()
-	defer store.Close()
 	TestGet(t, store)
 }
 
 // TestMemoryStoreDelete tests deleting an existing key for MemoryStore
 func TestMemoryStoreDelete(t *testing.T) {
 	store := NewMemoryStore()
-	defer store.Close()
 	TestDelete(t, store)
 }
 
 // TestMemoryStoreDeleteNonExistentKey tests deleting a non-existent key for MemoryStore
 func TestMemoryStoreDeleteNonExistentKey(t *testing.T) {
 	store := NewMemoryStore()
-	defer store.Close()
 	TestDeleteNonExistentKey(t, store)
 }
